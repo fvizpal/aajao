c412dabe85544be5dcc88b51770fc98b3a4bdc1d
fvizpal <vishalp9966@gmail.com>
2024-02-17 14:56:41 +0530

    UI changes

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,17 +1,15 @@
-
-import Collection from '@/components/shared/Collection';
-import ProfileBody from '@/components/shared/ProfileBody';
-import { Button } from '@/components/ui/button';
-import { getEventsByUser } from '@/lib/actions/event.actions';
-import { getOrdersByUser } from '@/lib/actions/order.actions';
-import { IOrder } from '@/lib/database/models/order.model';
-import { SearchParamProps } from '@/types';
+import Collection from '@/components/shared/Collection'
+import { Button } from '@/components/ui/button'
+import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
+import { SearchParamProps } from '@/types'
 import { auth } from '@clerk/nextjs'
-import Link from 'next/link';
-import React, { useState } from 'react'
+import Link from 'next/link'
+import React from 'react'
 
 const ProfilePage = async ({ searchParams }: SearchParamProps) => {
-  const { sessionClaims } = auth()
+  const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
 
   const ordersPage = Number(searchParams?.ordersPage) || 1;
@@ -20,10 +18,60 @@ const ProfilePage = async ({ searchParams }: SearchParamProps) => {
   const orders = await getOrdersByUser({ userId, page: ordersPage })
 
   const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
-  const organisedEvents = await getEventsByUser({ userId, page: eventsPage })
+  const organizedEvents = await getEventsByUser({ userId, page: eventsPage })
 
   return (
-    <ProfileBody orders={orders} ordersPage={ordersPage} eventsPage={eventsPage} orderedEvents={orderedEvents} organisedEvents={organisedEvents} />
+    <>
+      {/* My Tickets */}
+      <section>
+        <div>
+          <h3>My Tickets</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/#events">
+              Explore More Events
+            </Link>
+          </Button>
+        </div>
+      </section>
+
+      <section>
+        <Collection
+          data={orderedEvents}
+          emptyTitle="No event tickets purchased yet"
+          emptyStateSubText="Explore more events"
+          collectionType="My_Tickets"
+          limit={3}
+          page={ordersPage}
+          urlParamName="ordersPage"
+          totalPages={orders?.totalPages}
+        />
+      </section>
+
+      {/* Events Organized */}
+      <section>
+        <div>
+          <h3>Events Organized</h3>
+          <Button asChild size="lg" className="button hidden sm:flex">
+            <Link href="/events/create">
+              Create New Event
+            </Link>
+          </Button>
+        </div>
+      </section>
+
+      <section>
+        <Collection
+          data={organizedEvents?.data}
+          emptyTitle="No events have been created yet"
+          emptyStateSubText="Hop on and create some events now"
+          collectionType="Events_Organized"
+          limit={3}
+          page={eventsPage}
+          urlParamName="eventsPage"
+          totalPages={organizedEvents?.totalPages}
+        />
+      </section>
+    </>
   )
 }
 
