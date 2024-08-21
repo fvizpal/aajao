e921396f2ddd9ec0e5a8399ebe93e82a735e768a
fvizpal <vishalp9966@gmail.com>
2024-08-21 12:57:33 +0530

    fix:qs stringfy issue

--- lib/utils.ts
+++ lib/utils.ts
@@ -8,33 +8,33 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function formUrlQuery({ params, key, value }: UrlQueryParams) {
-  const currentUrl = qs.parse(params);
+  const currentUrl = qs.parse(params)
 
-  currentUrl[key] = value;
+  currentUrl[key] = value
 
-  return qs.stringify(
-    {
-      url: window.location.pathname,
-      query: currentUrl
-    },
-    { skipNull: true }
-  );
-}
-
-export function removeKeysFromQuery({ params, keysToRemove }: RemoveUrlQueryParams) {
-  const currentUrl = qs.parse(params);
-
-  keysToRemove.forEach(key => {
-    delete currentUrl[key];
-  })
-
-  return qs.stringify(
+  return qs.stringifyUrl(
     {
       url: window.location.pathname,
       query: currentUrl,
     },
     { skipNull: true }
-  );
+  )
+}
+
+export function removeKeysFromQuery({ params, keysToRemove }: RemoveUrlQueryParams) {
+  const currentUrl = qs.parse(params)
+
+  keysToRemove.forEach(key => {
+    delete currentUrl[key]
+  })
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: currentUrl,
+    },
+    { skipNull: true }
+  )
 }
 
 export const formatDateTime = (dateString: Date) => {
