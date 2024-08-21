69e2c3be104ef169ab913b16cbf45ae5798ce1a9
fvizpal <vishalp9966@gmail.com>
2024-08-21 13:10:21 +0530

    refactor: redirect to github

--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -1,16 +1,18 @@
 
+import Link from "next/link";
+
 const Footer = () => {
   return (
-    <footer className="border-t flex justify-center items-center lg:mx-auto p-4 md:px-10 w-full gap-4 text-center">
+    <footer className="border-t font-mono flex justify-center items-center lg:mx-auto p-4 md:px-10 w-full gap-4 text-center">
       <div>
         All right reserved.
       </div>
-      <div
-        onClick={() => window.open('https://github.com/fvizpal', '_blank')}
-        style={{ cursor: 'pointer' }}
+      <Link
+        href={"https://github.com./fvizpal"}
+        target="__blank"
       >
         @fvizpal
-      </div>
+      </Link>
     </footer>
   )
 }
